b8aa0dc0d523df70795d7e7ca58483288e96edfb
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-29 18:54:11 +0530

    Added: Custom Exception and Test case for wrong file

--- src/test/java/com/bridgelabz/CensusAnalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyser/CensusAnalyserTest.java
@@ -16,4 +16,16 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
       }
+
+      @Test
+      public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+            try {
+                  StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+                  ExpectedException exceptionRule = ExpectedException.none();
+                  exceptionRule.expect(CensusAnalyserException.class);
+                  censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+            } catch (CensusAnalyserException e) {
+                  Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+            }
+      }
 }
