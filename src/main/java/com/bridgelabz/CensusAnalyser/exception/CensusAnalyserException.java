34c891caad9d2ed5c9733f8574d9ab16656ad159
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-29 18:51:59 +0530

    Added: Test case for number of records

--- src/main/java/com/bridgelabz/CensusAnalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyser/exception/CensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.CensusAnalyser.exception;
 
 public class CensusAnalyserException extends RuntimeException {
       public enum ExceptionType {
-            UNABLE_TO_PARSE, NO_CENSUS_DATA, INVALID_COUNTRY, CENSUS_FILE_PROBLEM
+           CENSUS_FILE_PROBLEM
       }
       public ExceptionType type;
       public CensusAnalyserException(String message, ExceptionType type) {
