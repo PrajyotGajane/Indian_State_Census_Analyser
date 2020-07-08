286c918ffb58784600dd5cf578f94c5c6e13c571
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-08 10:18:54 +0530

    Refactored: Mockito test cases

--- src/main/java/com/bridgelabz/CensusAnalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyser/exception/CensusAnalyserException.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.CensusAnalyser.exception;
 public class CensusAnalyserException extends RuntimeException {
       public enum ExceptionType {
-           CENSUS_FILE_PROBLEM,WRONG_HEADER_FILE_OR_DELIMITER,DELIMITER_MISSING, INVALID_COUNTRY, UNABLE_TO_PARSE
+           CENSUS_FILE_PROBLEM,WRONG_HEADER_FILE_OR_DELIMITER, INVALID_COUNTRY
       }
       public ExceptionType type;
       public CensusAnalyserException(String message, ExceptionType type) {
--- src/test/java/com/bridgelabz/CensusAnalyser/CensusAnalyserMockitoTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyser/CensusAnalyserMockitoTest.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.CensusAnalyser;
-
+import com.bridgelabz.CensusAnalyser.adaptor.CensusAdaptor;
+import com.bridgelabz.CensusAnalyser.adaptor.IndiaCensusAdaptor;
+import com.bridgelabz.CensusAnalyser.adaptor.USCensusAdaptor;
 import com.bridgelabz.CensusAnalyser.controller.StateCensusAnalyser;
 import com.bridgelabz.CensusAnalyser.exception.CensusAnalyserException;
 import com.bridgelabz.CensusAnalyser.dao.CensusDAO;
@@ -7,22 +9,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 public class CensusAnalyserMockitoTest {
       private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
       private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
       private static final String US_CENSUS_CSV_FILE_PATH = "./src/test/resources/USCensusData.csv";
       Map<String, CensusDAO> censusDAOMap;
-      //Map<SortByField.Parameter, Comparator> sortParameterComparator;
-
-//@Rule
-//public MockitoRule mockitoRule = MockitoJUnit.rule();
       @Before
       public void setUp() {
             this.censusDAOMap = new HashMap<>();
@@ -31,6 +26,7 @@ public class CensusAnalyserMockitoTest {
             this.censusDAOMap.put("Karnataka", new CensusDAO("Karnataka", 7689699, 7686.8, 7568.8, "KA"));
             MockitoAnnotations.initMocks(this);
       }
+
       @Test
       public void givenIndiaCensusCSVFile_ShouldReturnCorrectRecords() {
             try {
@@ -38,6 +34,37 @@ public class CensusAnalyserMockitoTest {
                   when(censusAnalyser.loadCensusData(StateCensusAnalyser.Country.INDIA, INDIA_CENSUS_CSV_FILE_PATH, INDIA_STATE_CODE_CSV_FILE_PATH)).thenReturn(this.censusDAOMap.size());
                   int censusRecords = censusAnalyser.loadCensusData(StateCensusAnalyser.Country.INDIA, INDIA_CENSUS_CSV_FILE_PATH, INDIA_STATE_CODE_CSV_FILE_PATH);
                   Assert.assertEquals(3, censusRecords);
+            } catch (CensusAnalyserException e) {
+                  e.printStackTrace();
+            }
+      }
+
+      @Test
+      public void givenUSCensusData_withMockito_ShouldReturnCorrectRecord() {
+            StateCensusAnalyser censusAnalyser = mock(StateCensusAnalyser.class);
+            when(censusAnalyser.loadCensusData(StateCensusAnalyser.Country.US, US_CENSUS_CSV_FILE_PATH)).thenReturn(51);
+            int numOfState = censusAnalyser.loadCensusData(StateCensusAnalyser.Country.US, US_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(51, numOfState);
+      }
+
+      @Test
+      public void givenIndiaCensusAdaptorMock_ShouldReturnCorrectRecords() {
+            try {
+                  CensusAdaptor censusAdapter = mock(IndiaCensusAdaptor.class);
+                  when(censusAdapter.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH, INDIA_STATE_CODE_CSV_FILE_PATH)).thenReturn(this.censusDAOMap);
+                  Map<String, CensusDAO> censusDAOMapActual = censusAdapter.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH, INDIA_STATE_CODE_CSV_FILE_PATH);
+                  Assert.assertEquals(3, censusDAOMapActual.size());
+            } catch (CensusAnalyserException e) {
+                  e.printStackTrace();
+            }
+      }
+      @Test
+      public void givenUSCensusAdaptorMock_ShouldReturnCorrectRecords() {
+            try {
+                  CensusAdaptor censusAdapter = mock(USCensusAdaptor.class);
+                  when(censusAdapter.loadCensusData(US_CENSUS_CSV_FILE_PATH)).thenReturn(this.censusDAOMap);
+                  Map<String, CensusDAO> censusDAOMapActual = censusAdapter.loadCensusData(US_CENSUS_CSV_FILE_PATH);
+                  Assert.assertEquals(3, censusDAOMapActual.size());
             } catch (CensusAnalyserException e) {
                   e.printStackTrace();
             }
